a34980300808cd78ba2cf5fc7f5acfde8f89ee79
socialgreenhouse <socialgreenhouse@hotmail.com>
2013-06-21 13:00:11 +0200

    Revert "initial commit"
    This reverts commit 6939697185f8ddf6116b8468366ee4b27e3e8a12.

--- Android/gen/com/socialgreenhouse/android/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.socialgreenhouse.android;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Server/Code/SocialGreenhouseServer/run.bat
+++
@@ -1,2 +0,0 @@
-vertx run src/com/socialgreenhouse/server/Server.java -cp lib/mysql.jar;lib/twitter4j.jar
-pause
